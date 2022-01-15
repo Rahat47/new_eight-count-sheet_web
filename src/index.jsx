17a23b1283f9a5b8af5926395f3b0a5ad5e79078
Rahat47 <djrayhan8@gmail.com>
2022-01-15 22:38:19 +0600

    initialize a new app with react redux chakra ui and much more

---
+++ src/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { store } from './app/store';
+import { Provider } from 'react-redux';
+import { ChakraProvider } from '@chakra-ui/react';
+import './index.css';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <ChakraProvider resetCSS>
+                <App />
+            </ChakraProvider>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
